55633190f17f9432b00e80ad141834563b7ba539
! Ｄᴇᴠɪʟɪѕʜ ｃʜʀᴏɴɪᴄʟᴇѕ <133841052+Darknessking13@users.noreply.github.com>
2024-11-03 22:25:26 +0600

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,6 @@
 module.exports = {
     serverIP: 'localhost',
     port: 8080,
-    username: 'admin',
-    password: 'adminpassword',
-    subUsers: [
-        { username: 'user1', password: 'pass1' },
-        { username: 'user2', password: 'pass2' }
-    ],
     ipWhitelist: false,
     whitelistedIps: ['127.0.0.1'],
     ipBlacklist: false,
